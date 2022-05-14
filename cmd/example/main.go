b8b212a46bb4e08b5987abbb63b56738332fc123
Matthew <ikirik59@gmail.com>
2022-05-14 13:30:17 +0300

    Add temporary test calls

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+
+	lab2 "github.com/roman-mazur/architecture-lab-2"
 )
 
 var (
@@ -11,5 +13,10 @@ var (
 
 func main() {
 	flag.Parse()
-
+	lab2.PrefixToInfix("/ * / 22 12 44 * 1 + 10 1")         // 22 / 12 * 44 / (1 * (10 + 1))
+	lab2.PrefixToInfix("/ / / 12 12 12 12")                 // 12 / 12 / 12 / 12
+	lab2.PrefixToInfix("+ / 99 + 51 1 12")                  // 99 / (51 + 1) + 12
+	lab2.PrefixToInfix("+ + ^ 12 1 / 122 1 * * 11 2 - 1 0") // 12^1 + 122 / 1 + 11 * 2 * (1 - 0)
+	lab2.PrefixToInfix("^ 55 + - + * 12 12 / 12 12 1 2")    // 55 ^ (12 * 12 + 12 / 12 - 1 + 2)
+	lab2.PrefixToInfix("- - + 12 111 + 12 1 - 10 2")        // ((12+111)) - (12 + 1) - (10 - 2)
 }
