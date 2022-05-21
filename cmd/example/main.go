9a923c7144818741816348bf5c4a2f1bfa4b20e1
Ivan Baturkin <ivanmysterys@gmail.com>
2022-05-21 17:44:11 +0300

    feat: add errors handling basic struct

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,24 +2,62 @@ package main
 
 import (
 	"flag"
-
-	lab2 "github.com/MatthewKirik/architecture-lab-2"
+	"strings"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputExpression = flag.String("e", "xx", "Prefix expression that should be converted")
+	inputFile       = flag.String("f", "", "Input file with prefix expression")
+	outputFile      = flag.String("o", "", "Result file with infix expression")
 )
 
 func main() {
 	flag.Parse()
-	lab2.PrefixToInfix("/ * / 22 12 44 * 1 + 10 1")              // 22 / 12 * 44 / (1 * (10 + 1))
-	lab2.PrefixToInfix("/ / / 12 12 12 12")                      // 12 / 12 / 12 / 12
-	lab2.PrefixToInfix("+ / 99 + 51 1 12")                       // 99 / (51 + 1) + 12
-	lab2.PrefixToInfix("+ + ^ 12 1 / 122 1 * * 11 2 - 1 0")      // 12^1 + 122 / 1 + 11 * 2 * (1 - 0)
-	lab2.PrefixToInfix("^ 55 + - + * 12 12 / 12 12 1 2")         // 55 ^ (12 * 12 + 12 / 12 - 1 + 2)
-	lab2.PrefixToInfix("- - + 12 111 + 12 1 - 10 2")             // ((12+111)) - (12 + 1) - (10 - 2)
-	lab2.PrefixToInfix("- + 5 / 9 2 / - + 1 7 / + 8 4 2 3")      // 5 + (9 / 2) - ((1 + 7) - (8 + 4) / 2) / 3
-	lab2.PrefixToInfix("- + a / b 2 / - + 1 b / + MAKAKA n 2 3") // a + b / 2 - (1 + b - (MAKAKA + n) / 2) / 3
-	lab2.PrefixToInfix("- + a / b 2 / - + 1 b / sin n 2 3")      // a + b / 2 - (1 + b - sin(n) / 2) / 3
+
+	*inputExpression = strings.TrimSpace(*inputExpression)
+	*inputFile = strings.TrimSpace(*inputFile)
+	*outputFile = strings.TrimSpace(*outputFile)
+
+	// inputIsNotSpecified = len(*inputFile) == 0 &&
+	// 	len(*inputExpression) == 0
+	// if inputIsNotSpecified {
+	// 	// TODO: print error
+	// }
+
+	// manyInputsSpecified := len(*inputFile) > 0 &&
+	// 	len(*inputExpression) > 0
+	// if manyInputsSpecified {
+	// 	// TODO: print error
+	// }
+
+	if *inputExpression != "" && *inputFile != "" {
+		// TODO: print error many inputs specified
+
+	} else if *inputExpression != "" && *inputFile == "" {
+		// TODO create new reader
+
+	} else if *inputExpression == "" && *inputFile != "" {
+		// try open input file, handle error
+
+	} else {
+		// TODO: Print error no input was specified
+
+	}
+
+	if *outputFile != "" {
+		// Create output file
+
+	} else {
+		// Set defaut output as STDOUT
+
+	}
+
+	// fmt.Printf("value = \"%s\"\n", *inputExpression)
+	// fmt.Printf("value = \"%s\"\n", *inputFile)
+	// fmt.Printf("value = \"%s\"\n", *outputFile)
+
+	// prints to stderr
+	// fmt.Fprintf(os.Stderr, "number of foo: %d", 1)
+	// fmt.Fprintln(os.Stderr, "hello world")
+
 }
